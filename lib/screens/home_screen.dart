b9965e79e44fbce6b5bdff6280fdda4cfac8509d
Isaac Onah <iumeha@yahoo.com>
2023-01-11 09:29:54 +0100

    recent update

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,7 @@
+import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:ticket/util/style.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -11,20 +14,23 @@ class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Styles.bgColor,
       body: ListView(
         children: [
           Container(
             padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
             child: Column(
               children: [
+                const Gap(40),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.spaceBetween,
                   children: [
                     Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        Text('Good Morning') ,
-                        Text('Book Tickets'),
+                        Text('Good Morning', style: Styles.headLineStyle3,) ,
+                        const Gap(5),
+                        Text('Book Tickets', style: Styles.headLineStyle,),
                       ],
                     ) ,
                     Container(
@@ -36,6 +42,20 @@ class _HomeScreenState extends State<HomeScreen> {
                       ),
                     )
                   ],
+                ),
+                const Gap(25),
+                Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  child: Row(
+                    children: [
+                      const Icon(FluentSystemIcons.ic_fluent_search_filled, color: Color(0xFFBFC205),),
+                      Text('Search', style: Styles.headLineStyle4,),
+                    ],
+                  ),
                 )
               ],
             ),
